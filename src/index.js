01428602c20c93e0c25a667b457ee4d72a146a48
brianleroux <b@brian.io>
2015-12-19 16:11:54 -0800

    in progress adding to describe method

--- src/index.js
+++ src/index.js
@@ -21,13 +21,83 @@ let describe = `
   slack
     api.test(params, (err, data)=>)
     auth.test(token, (err, data)=>)
-    oauth.access({client_id, client_secret, code}, (err, data)=>)
-    team.info(token, (err, data)=>)
-    users.list(token, (err, data)=>)
-    channels.list({token, exclude_archived}, (err, data)=>)
+    channels.archive
+    channels.create
     channels.history({token, channel}, (err, data)=>)
-    rtm.start({token}, (err, data)=>)
+    channels.info
+    channels.invite
+    channels.join
+    channels.kick
+    channels.leave
+    channels.list({token, exclude_archived}, (err, data)=>)
+    channels.mark
+    channels.rename
+    channels.setPurpose
+    channels.setTopic
+    channels.unarchive
+    chat.delete
     chat.postMessage({token, text, channel}, (err, data)=>)
+    chat.update
+    emoji.list
+    files.delete
+    files.info
+    files.list
+    files.upload
+    groups.archive
+    groups.close
+    groups.create
+    groups.createChild
+    groups.history
+    groups.info
+    groups.invite
+    groups.kick
+    groups.leave
+    groups.list
+    groups.mark
+    groups.open
+    groups.rename
+    groups.setPurpose
+    groups.setTopic
+    groups.unarchive
+    im.close
+    im.history
+    im.list
+    im.mark
+    im.open
+    mpim.close
+    mpim.history
+    mpim.list
+    mpim.mark
+    mpim.open
+    oauth.access({client_id, client_secret, code}, (err, data)=>)
+    pins.add
+    pins.list
+    pins.remove
+    reactions.add
+    reactions.get
+    reactions.list
+    reactions.remove
+    rtm.start({token}, (err, data)=>)
+    search.all
+    search.files
+    search.messages
+    stars.add
+    stars.list
+    stars.remove
+    team.acccessLogs
+    team.info(token, (err, data)=>)
+    usergroups.create
+    usergroups.disable
+    usergroups.enable
+    usergroups.list
+    usergroups.update
+    usergroups.users.list
+    usergroups.users.update
+    users.getPresence
+    users.info
+    users.list(token, (err, data)=>)
+    users.setActive
+    users.setPresence
 `
 
 export default {

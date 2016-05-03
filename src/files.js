fa447817da5bf0b2a4b29d73a1ee69c9645919c0
brianleroux <b@brian.io>
2016-05-03 15:20:39 -0700

    adding reminders and file commenting api stuff

--- src/files.js
+++ src/files.js
@@ -3,9 +3,24 @@ import info from './files.info'
 import list from './files.list'
 import upload from './files.upload'
 
+import filesCommentsAdd from './files.comments.add'
+import filesCommentsDelete from './files.comments.delete'
+import filesCommentsEdit from './files.comments.edit'
+import revokePublicURL from './files.revokePublicURL'
+import sharedPublicURL from './files.sharedPublicURL'
+
 export default {
-  delete:del,
+  delete: del,
   info,
   list,
-  upload
+  upload,
+  files: {
+    comments: {
+      add: filesCommentsAdd,
+      delete: filesCommentsDelete,
+      edit: filesCommentsEdit
+    },
+    revokePublicURL,
+    sharedPublicURL
+  }
 }
---
+++ src/reminders.js
@@ -0,0 +1,10 @@
+import add from './reminders.add'
+import complete from './reminders.complete'
+import del from './reminders.delete'
+import info from './reminders.info'
+import list from './reminders.list'
+
+export default {
+  add, complete, delete:del, info, list
+}
+

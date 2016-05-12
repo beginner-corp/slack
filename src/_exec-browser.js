9e03d8c5e801a27364303180dd0a552a2e109bc7
brianleroux <b@brian.io>
2016-05-12 10:25:58 -0700

    fix for a fuzzy rate limit error

--- src/_exec-browser.js
+++ src/_exec-browser.js
@@ -6,11 +6,20 @@ export default function exec(ns, json, callback) {
   let query = qs.stringify(json)
   let baseUrl = 'https://slack.com/api/'
   let url = `${baseUrl}${ns}?${query}`
+  var rateLimit = 'You are sending too many requests. Please relax.'
 
   http.get(url, (err, res)=> {
     if (err) {
       // if request failed bubble the error
       callback(err)
+    }
+    else if (res.body.error) {
+      // if Slack returns an error bubble the error
+      callback(Error(res.body.error))
+    }
+    else if (typeof res.body === 'string' && res.body.includes(rateLimit)) {
+      // sometimes you need to chill out
+      callback(Error('rate_limit'))
     }
     else {
       // success! clean up the response
--- src/_exec.js
+++ src/_exec.js
@@ -12,6 +12,7 @@ export default function exec(url, form, callback) {
 
   // always post
   req.post({url, form}, (err, res)=> {
+    var rateLimit = 'You are sending too many requests. Please relax.'
     if (err) {
       // if request failed bubble the error
       callback(err)
@@ -19,6 +20,10 @@ export default function exec(url, form, callback) {
     else if (res.body.error) {
       // if Slack returns an error bubble the error
       callback(Error(res.body.error))
+    }
+    else if (typeof res.body === 'string' && res.body.includes(rateLimit)) {
+      // sometimes you need to chill out
+      callback(Error('rate_limit'))
     }
     else {
       // success! clean up the response

241553cf568c4d3af40ad9a4ce74e8ec3e1a3be3
Mike Brevoort <mike@brevoort.com>
2016-06-27 17:23:18 -0600

    JSON.stringify any object value in POST of form

--- test/chat.postMessage-test.js
+++ test/chat.postMessage-test.js
@@ -27,3 +27,27 @@ test('can post a message', t=> {
     })
   })
 })
+
+test('can post a message with attachment as array', t=> {
+  let token = process.env.SLACK_BOT_TOKEN
+  let text = 'test message'
+  // list channels
+  list({token}, (err, json)=> {
+    // look for a channel called 'test'
+    let channel = json.channels.filter(c=> c.name === 'test')[0].id
+    let attachments = [{ text: "feeling attached" }]
+    let params = {token, text, channel, attachments}
+    // post a message there
+    msg(params, (err, data)=> {
+      if (err) {
+        t.fail(err, 'chat.postMessage fails')
+        console.error(err)
+      }
+      else {
+        t.ok(data, 'posted a message')
+        console.log(data)
+      }
+      t.end()
+    })
+  })
+})

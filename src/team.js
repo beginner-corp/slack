bcfc91e04c86ad144341b0dab131318aa33ed56e
Robert Ikeoka <rikeoka@zendesk.com>
2016-09-21 23:17:33 -0700

    Add support for all of the team functions
    Client was not loading billableInfo, IntegrationLogs and profile.get
    for teams.

--- src/team.js
+++ src/team.js
@@ -1,7 +1,15 @@
 import accessLogs from './team.accessLogs'
+import billableInfo from './team.billableInfo'
 import info from './team.info'
+import integrationLogs from './team.integrationLogs'
+import profileGet from './team.profile.get'
 
 export default {
-  accessLogs, 
-  info
+  accessLogs,
+  billableInfo,
+  info,
+  integrationLogs,
+  profile: {
+    get: profileGet
+  }
 }

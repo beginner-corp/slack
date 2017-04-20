28f87711a65399124e70272402a94022af4920f3
Brian LeRoux <b@brian.io>
2017-04-20 14:19:51 -0700

    adds new rtm.connect api into api ;P

--- src/index.js
+++ src/index.js
@@ -16,109 +16,14 @@ import reminders from './reminders'
 import pins from './pins'
 import client from './rtm.client'
 import start from './rtm.start'
+import connect from './rtm.connect'
 import search from './search'
 import stars from './stars'
 import team from './team'
 import usergroups from './usergroups'
 import users from './users'
 
-let describe = `
-  slack
-    api.client(token)
-    api.test(params, (err, data)=>)
-    auth.test(token, (err, data)=>)
-    bots.info
-    channels.archive({token, channel}, (err, data)=>)
-    channels.create({token, name}, (err, data)=>)
-    channels.history({token, channel}, (err, data)=>)
-    channels.info
-    channels.invite
-    channels.join
-    channels.kick
-    channels.leave
-    channels.list({token, exclude_archived}, (err, data)=>)
-    channels.mark
-    channels.rename
-    channels.setPurpose
-    channels.setTopic
-    channels.unarchive
-    chat.delete
-    chat.postMessage({token, text, channel}, (err, data)=>)
-    chat.update
-    emoji.list
-    files.delete
-    files.info
-    files.list
-    files.upload
-    groups.archive
-    groups.close
-    groups.create
-    groups.createChild
-    groups.history
-    groups.info
-    groups.invite
-    groups.kick
-    groups.leave
-    groups.list
-    groups.mark
-    groups.open
-    groups.rename
-    groups.setPurpose
-    groups.setTopic
-    groups.unarchive
-    im.close
-    im.history
-    im.list
-    im.mark
-    im.open
-    mpim.close
-    mpim.history
-    mpim.list
-    mpim.mark
-    mpim.open
-    oauth.access({client_id, client_secret, code}, (err, data)=>)
-    pins.add
-    pins.list
-    pins.remove
-    reactions.add
-    reactions.get
-    reactions.list
-    reactions.remove
-    reminders.add
-    reminders.complete
-    reminders.delete
-    reminders.info
-    reminders.list
-    rtm.client()
-    rtm.start({token}, (err, data)=>)
-    search.all
-    search.files
-    search.messages
-    stars.add
-    stars.list
-    stars.remove
-    team.acccessLogs
-    team.billableInfo
-    team.info(token, (err, data)=>)
-    team.integrationLogs
-    team.profile.get
-    usergroups.create
-    usergroups.disable
-    usergroups.enable
-    usergroups.list
-    usergroups.update
-    usergroups.users.list
-    usergroups.users.update
-    users.getPresence
-    users.identity
-    users.info
-    users.list(token, (err, data)=>)
-    users.setActive
-    users.setPresence
-`
-
 export default {
-  describe,
   api: {test:api, client:apiClient},
   auth: {test},
   bots,
@@ -134,7 +39,7 @@ export default {
   reactions,
   reminders,
   pins,
-  rtm: {client, start},
+  rtm: {client, start, connect},
   search,
   stars,
   team,

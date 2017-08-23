033e801590da84fd80717198d1960742d5d94a8f
abhaysingh1722 <abhaysingh1722@yahoo.in>
2017-08-23 14:17:55 +0530

    Update channels.list-test.js

--- test/channels.list-test.js
+++ test/channels.list-test.js
@@ -3,20 +3,39 @@ import list from '../src/channels.list'
 import env from './_env'
 // load SLACK_BOT_TOKEN for testing
 env()
-
+var fs = require('fs');
 test('can list channels', t=> {
   let token = process.env.SLACK_BOT_TOKEN
   let exclude_archived = 1
   let params = {token, exclude_archived}
-  list(params, (err, data)=> {
+  list(params, (err, json)=> {
     if (err) {
       t.fail(err, 'channels.list fails')
       console.error(err)
     }
     else {
-      t.ok(data, 'listed channels')
-      console.log(data)
+      //t.ok(data, 'listed channels')
+      //console.log(json)
+      let channel = json.channels.filter(c=> c.name === 'daily_status')[0].id
+      let params = {token, channel}
+      list(params, (err, data)=> {
+        if (err) {
+          t.fail(err, 'channels.list fails')
+          console.error(err)
+        }
+        else {
+          t.ok(data, 'listed channels')
+          console.log(data)
+          var jsonData=JSON.stringify(data,null,'\t')    
+          fs.writeFile("channel_list.json",jsonData, function(err) {
+            if(err) {
+              return console.log(err);
+            }
+          });
+        }
+        t.end()
+      })
     }
-    t.end()
   })
 })
+

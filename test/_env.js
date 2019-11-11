4940b149b13eb25a4d30832bbec483aa74bd5b2e
Ryan Block <rblock@gmail.com>
2019-11-11 15:17:26 -0800

    Load env file conditionally if env vars not present

--- test/_env.js
+++ test/_env.js
@@ -3,9 +3,11 @@ var path = require('path')
 
 // loads .env variables to test locally
 module.exports = function envForTesting() {
-  let mode = process.env.NODE_ENV
+  let env = process.env
+  let mode = env.NODE_ENV
   let isDev = typeof mode === 'undefined' || mode === 'development'
-  if (isDev) {
+  let hasVars = env.SLACK_BOT_TOKEN && env.SLACK_CLIENT_ID && env.SLACK_CLIENT_SECRET
+  if (isDev && !hasVars) {
     env(path.join(process.cwd(), '.env'))
   }
 }

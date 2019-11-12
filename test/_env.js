b6c42477a0cba95b0da807dacf7a7e78a06232c8
Ryan Block <rblock@gmail.com>
2019-11-11 16:27:35 -0800

    Update the rest of the local env testing predicates

--- test/_env.js
+++ test/_env.js
@@ -3,12 +3,10 @@ var path = require('path')
 
 // loads .env variables to test locally
 module.exports = function envForTesting() {
-  let mode = process.env.NODE_ENV
-  let isDev = typeof mode === 'undefined' || mode === 'development'
   let hasVars = process.env.SLACK_BOT_TOKEN &&
                 process.env.SLACK_CLIENT_ID &&
                 process.env.SLACK_CLIENT_SECRET
-  if (isDev && !hasVars) {
+  if (!hasVars) {
     env(path.join(process.cwd(), '.env'))
   }
 }
